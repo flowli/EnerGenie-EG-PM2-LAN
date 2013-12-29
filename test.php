1c4ba82ff1a2e2fbc4b4be2bb51c8e08eb66cd3f
Florian Arndt <post@florianarndt.com>
2013-12-29 10:08:37 +0100

    Added comments and usage of getStatus

--- test.php
+++ test.php
@@ -1,9 +1,22 @@
 <?php
 require_once dirname(__FILE__) . '/class.EnerGenieSwitcher.php';
+
+/**
+ * Initialize with connection parameters (ip address and credentials)
+ */
 $egs = new EnerGenieSwitcher('1.2.3.4', 'my-password');
+
+/**
+ * Switch port statuses
+ */
 $egs->doSwitch(array(
 	1 => EnerGenieSwitcher::ON,
 	2 => EnerGenieSwitcher::OFF,
 	3 => EnerGenieSwitcher::ON,
 	4 => EnerGenieSwitcher::OFF
 ));
+
+/**
+ * Verify the status of the ports
+ */
+var_dump($egs->getStatus());
